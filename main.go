52d0afeecf64dd30631ceabad67965fe0e530770
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2024-04-23 02:28:32 +0300

    using instruction to deploy #1

--- main.go
+++ main.go
@@ -1,16 +1,60 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"log"
 	"net/http"
+
+	"golang.org/x/crypto/acme/autocert"
+	"golang.org/x/sync/errgroup"
 )
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, World!")
+	fmt.Fprintf(w, "Hello, World!")
 }
 
 func main() {
-    http.HandleFunc("/", helloWorldHandler)
-    fmt.Println("Server is running on port 8080...")
-    http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", helloWorldHandler)
+	run(mux)
+}
+
+func run(handler http.Handler) {
+	runProduction(handler)
+}
+
+// https://stackoverflow.com/questions/37321760/how-to-set-up-lets-encrypt-for-a-go-server-application
+// https://stackoverflow.com/a/40494806/17655004
+func runProduction(handler http.Handler) {
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("shortener.u8views.com", "dev.shortener.u8views.com"),
+		Cache:      autocert.DirCache("/var/lib/u8views/go-url-shortener/tls-certificates/"),
+	}
+
+	server := &http.Server{
+		Addr:    ":https",
+		Handler: handler,
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+			MinVersion:     tls.VersionTLS12, // improves cert reputation score at https://www.ssllabs.com/ssltest/
+		},
+	}
+
+	var g errgroup.Group
+
+	g.Go(func() error {
+		return http.ListenAndServe(":http", certManager.HTTPHandler(nil))
+	})
+
+	g.Go(func() error {
+		return server.ListenAndServeTLS("", "") // Key and cert are coming from Let's Encrypt
+	})
+
+	log.Fatal(g.Wait())
+}
+
+func runDevelopment(handler http.Handler) {
+	log.Fatal(http.ListenAndServe(":http", handler))
 }
